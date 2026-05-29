d009e0460c6cce14f3c60adbed7fd618861720d6
jiazhiyaun <jiazhiyuan@sto.cn>
2026-05-29 17:53:14 +0800

    feat: 接入域名

--- apps/mini/config/prod.ts
+++ apps/mini/config/prod.ts
@@ -2,7 +2,7 @@ import type { UserConfigExport } from "@tarojs/cli";
 
 const config = {
   env: {
-    TARO_APP_API_BASE_URL: "https://admin.example.com"
+    TARO_APP_API_BASE_URL: "https://landongdj.cn"
   },
   defineConstants: {}
 } as unknown as UserConfigExport;
