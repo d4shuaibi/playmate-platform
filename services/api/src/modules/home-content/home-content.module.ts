e55db74eda9ebbab5040b1a2a07a2e979b950031
jiazhiyaun <jiazhiyuan@sto.cn>
2026-06-09 16:06:13 +0800

    fix: 修复部署bug

--- services/api/src/modules/home-content/home-content.module.ts
+++ services/api/src/modules/home-content/home-content.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { HomeContentService } from "./home-content.service";
 import { HomeContentController } from "./home-content.controller";
 import { MiniHomeContentController } from "./mini-home-content.controller";
 
 @Module({
+  imports: [AuthModule],
   controllers: [HomeContentController, MiniHomeContentController],
   providers: [HomeContentService],
   exports: [HomeContentService]
